f7e9c98636bbb2af5b70acb3cfa7f094acbc734b
JeffL1005 <70871864+JeffL1005@users.noreply.github.com>
2022-12-19 17:20:39 -0500

    Modify message for NewChirp notification

--- app/Notifications/NewChirp.php
+++ app/Notifications/NewChirp.php
@@ -43,8 +43,10 @@ class NewChirp extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
+                    ->subject("New Chirp from {$this->chirp->user->name}")
+                    ->greeting("New Chirp from {$this->chirp->user->name}")
+                    ->line(Str::limit($this->chirp->message, 50))
+                    ->action('Go to Chirper', url('/'))
                     ->line('Thank you for using our application!');
     }
 
